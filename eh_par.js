397d2783b841ecc2aaa1266614afa6c428efc899
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-31 13:11:38 -0300

    Interação com o usuário pelo terminal

--- eh_par.js
+++ eh_par.js
@@ -2,9 +2,31 @@ console.log("Crie um programa que seja capaz de percorrer uma lista de números 
 
 var numero = [];
 
+const readline = require(`node:readline`);
+const {stdin: input, stdout: output} = require(`node:process`);
+
+const leitor = readline.createInterface({
+    input,
+    output
+});
+
+leitor.question("Digite um número a ser inserido na lista: \n", (answer1) =>{
+    leitor.question("Digite um número a ser inserido na lista: \n", (answer2) =>{
+        leitor.question("Digite um número a ser inserido na lista: \n", (answer3) =>{
+            leitor.question("Digite um número a ser inserido na lista: \n", (answer4) =>{
+                leitor.question("Digite um número a ser inserido na lista: \n", (answer5) =>{
+                   numero = [parseInt(answer1, 10), parseInt(answer2, 10), parseInt(answer3, 10), parseInt(answer4, 10), parseInt(answer5, 10)];
+                   console.log(numero);
+                });
+            });
+        });
+    });
+});
 
 
 
-for(let i = 0; i < 10; i++){
-    
-};
+/*for(let i = 0; i < 5; i++){
+    if(numero[i] % 2 == 0){
+        console.log(numero[i]);
+    }
+};*/
