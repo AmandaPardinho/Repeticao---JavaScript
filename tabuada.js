d6b33063b1594141c1169c2ee2003898c4f6a487
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-31 11:23:56 -0300

    Resolução do exercício

--- tabuada.js
+++ tabuada.js
@@ -1,8 +1,10 @@
 console.log("Crie um programa que, dado um número, imprima sua tabuada");
 
-var numero;
+var numero = 1;
 var tabuada;
 
 for(i = 1; i <= 10; i++){
     tabuada = numero * i;
+    console.log(numero + " x " + i + " = " + tabuada);
 }
+
