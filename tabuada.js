a0d662d38ec68133366ce93dcf8eeb5b79c13500
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-31 11:54:41 -0300

    Nova resolução do problema

--- tabuada.js
+++ tabuada.js
@@ -1,10 +1,22 @@
 console.log("Crie um programa que, dado um número, imprima sua tabuada");
 
-var numero = 1;
+//var numero = 1;
 var tabuada;
 
-for(i = 1; i <= 10; i++){
-    tabuada = numero * i;
-    console.log(numero + " x " + i + " = " + tabuada);
-}
+const readline = require(`node:readline`);
+const {stdin: input, stdout: output} = require(`node:process`);
+
+const leitor = readline.createInterface({
+    input,
+    output
+});
+
+leitor.question("Digite um número:\n", (answer1) =>{
+    var numero = answer1;
+    for(i = 1; i <= 10; i++){
+        tabuada = numero * i;
+        console.log(numero + " x " + i + " = " + tabuada);
+    }
+});
+
 
