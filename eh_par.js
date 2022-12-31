86de520d03187daee6d359b4588de1edded66f34
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-31 13:16:43 -0300

    Fim da resolução do exercício

--- eh_par.js
+++ eh_par.js
@@ -1,7 +1,5 @@
 console.log("Crie um programa que seja capaz de percorrer uma lista de números e imprima todo número par que for encontrado\n");
 
-var numero = [];
-
 const readline = require(`node:readline`);
 const {stdin: input, stdout: output} = require(`node:process`);
 
@@ -15,8 +13,13 @@ leitor.question("Digite um número a ser inserido na lista: \n", (answer1) =>{
         leitor.question("Digite um número a ser inserido na lista: \n", (answer3) =>{
             leitor.question("Digite um número a ser inserido na lista: \n", (answer4) =>{
                 leitor.question("Digite um número a ser inserido na lista: \n", (answer5) =>{
-                   numero = [parseInt(answer1, 10), parseInt(answer2, 10), parseInt(answer3, 10), parseInt(answer4, 10), parseInt(answer5, 10)];
-                   console.log(numero);
+                    var numero = [parseInt(answer1, 10), parseInt(answer2, 10), parseInt(answer3, 10), parseInt(answer4, 10), parseInt(answer5, 10)];
+                    //console.log(numero);
+                    for(let i = 0; i < 5; i++){
+                        if(numero[i] % 2 == 0){
+                            console.log(numero[i]);
+                        }
+                    };
                 });
             });
         });
@@ -24,9 +27,3 @@ leitor.question("Digite um número a ser inserido na lista: \n", (answer1) =>{
 });
 
 
-
-/*for(let i = 0; i < 5; i++){
-    if(numero[i] % 2 == 0){
-        console.log(numero[i]);
-    }
-};*/
